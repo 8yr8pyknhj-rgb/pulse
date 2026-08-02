3f086075dbbc560e16c7d159da4bb7d8b4130228
8yr8pyknhj-rgb <8yr8pyknhj@privaterelay.appleid.com>
2026-08-02 02:50:44 +0100

    Update ast.py

--- compiler/ast.py
+++ compiler/ast.py
@@ -1,1 +1,37 @@
-# ast.py
+# ==========================================
+# Pulse AST Nodes
+# ==========================================
+
+class Node:
+    pass
+
+
+class Program(Node):
+    def __init__(self, statements):
+        self.statements = statements
+
+
+class SayStatement(Node):
+    def __init__(self, value):
+        self.value = value
+
+
+class Assignment(Node):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+
+class Identifier(Node):
+    def __init__(self, name):
+        self.name = name
+
+
+class StringLiteral(Node):
+    def __init__(self, value):
+        self.value = value
+
+
+class NumberLiteral(Node):
+    def __init__(self, value):
+        self.value = value
